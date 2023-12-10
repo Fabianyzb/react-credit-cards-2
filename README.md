837efdd812609218d2a3a864457ace944d2d3d8c
Fabian Yanez <fabian.yzb@gmail.com>
2023-12-10 09:16:36 -0300

    first commit

---
+++ README.md
@@ -0,0 +1,5 @@
+REACT-CREDIT-CARDS-2 DOCUMENTATION:
+
+CLEAN AND SIMPLE
+
+https://www.npmjs.com/package/react-credit-cards-2
---
+++ src/main.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import PaymentForm from "./PaymentForm.jsx";
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <PaymentForm />
+  </React.StrictMode>
+);
